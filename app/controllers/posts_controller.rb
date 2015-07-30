e18e2ec919e1868e11d01a775fd33452af2157f6
facingsouth <xin.d.liu@gmail.com>
2015-07-30 11:43:50 -0700

    add nested comment form under post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,62 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
+    @post.comments.build
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to posts_path
+    else
+      flash.now[:error] = "Not albe to create post"
+      render :new
+    end
+  end
 
-    redirect_to new_post_path
+  def edit
+    @post = Post.find(params[:id])
+    @post.comments.build
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Your post is Updated!"
+      redirect_to posts_path
+    else
+      flash[:error] = "Oops! Something went wrong, try again!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:success] = "Deleted!"
+      redirect_to posts_path
+    else
+      flash[:error] = "Indestructible!"
+      redirect_to posts_path
+    end
   end
 
   def post_params
     params
       .require(:post)
-      .permit(:title, :body, :tags_on_authored_post_ids => [])
+      .permit(:title, 
+              :body, 
+              {:tags_on_authored_post_ids => []},
+              {:comments_attributes => [
+                  :body,
+                  :user_id,
+                  :id,
+                  :_destroy ] } )
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,10 @@ class Post < ActiveRecord::Base
   has_many :authors, :source => :user, :through => :user_posts
   has_many :post_taggings
   has_many :tags_on_authored_posts, :source => :tag, :through => :post_taggings
-  has_many :child_comments, 
-           :class_name => "Comment" , 
+  has_many :comments,
            :dependent => :destroy
+
+  accepts_nested_attributes_for :comments, 
+                                :reject_if => :all_blank,
+                                :allow_destroy => true
 end
